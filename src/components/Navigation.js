4eb665ea621a2ecd4630f801a0e45d4f627c0a6a
Marianna <marianna.janczak@applica.ai>
2019-08-07 15:00:37 +0200

    logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,21 +4,23 @@ export default {
     props: ['activeNav'],
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+                <a class="navbar-brand" href="#">
+                    <img src="./../logo.png"/>
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <router-link class="default-button" to="/welcome">Welcome</router-link>
+                        <li class="nav-item active px-3 py-2">
+                            <router-link class="btn btn-outline-secondary" to="/welcome">Welcome</router-link>
                         </li>
-                        <li class="nav-item active">
-                            <router-link class="default-button" to="/dashboard">Dashboard</router-link>
+                        <li class="nav-item active px-3 py-2">
+                            <router-link class="btn btn-outline-secondary" to="/dashboard">Dashboard</router-link>
                         </li>
-                        <li class="nav-item active">
-                            <router-link class="default-button" to="/competitions">Competitions</router-link>
+                        <li class="nav-item active px-3 py-2">
+                            <router-link class="btn btn-outline-secondary" to="/competitions">Competitions</router-link>
                         </li>
                     </ul>
                 </div>
