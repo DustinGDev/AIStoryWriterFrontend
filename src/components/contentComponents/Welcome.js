9cfced22b8fab0e3dd60b15bf572fef21e892e5b
Marianna <marianna.janczak@applica.ai>
2019-08-10 12:36:39 +0200

    hemingway quote

--- src/components/contentComponents/Welcome.js
+++ src/components/contentComponents/Welcome.js
@@ -7,11 +7,13 @@ export default {
     render() {
         return (
             <div class="row">
-                <div class="col-sm-9" id="welcome-logo">
-                </div>
-                <div class="col-sm-3">
-                <h1>Welcome</h1>
-                <p>To the AI story writing competition App! What is this all about you ask? This is about writing collaborative
+                <div class="col-sm-8" id="welcome-logo"></div>
+                <div class="col-sm-4  px-3 py-5">
+                <blockquote class="blockquote">
+                    <p class="mb-0" id="quote">“The first draft of anything is shit.” </p>
+                    <footer class="blockquote-footer float-right"><cite title="Source Title">Ernest Hemingway</cite></footer>
+                </blockquote>
+                <p>Welcome to the AI story writing competition App! What is this all about you ask? This is about writing collaborative
                 stories, either as person or with a Neural Network. Therefore you probably belong to one of two groups:</p>
                 <TextList listObject={['Writes', 'ML Professionals/Enthusiasts', 'Everyone else?']}>
                     <TextView>
